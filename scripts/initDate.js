6aff1a93e6d49debbe9ce5f887d400f4eaf3b7ff
noecor <noeliacoronelg@hotmail.com>
2020-07-10 12:52:04 -0300

    intento class component

--- scripts/initDate.js
+++ scripts/initDate.js
@@ -1,9 +1,26 @@
 
 const InitDate = () => {
-  const today = new Date().toDateString();
+
     return (
       <div>
+        <input type="date"></input>
         <input type="date"></input>
       </div>
     );
   }
+
+/* no funciona class:
+class InitDate extends React.Component {
+  state= {
+    initialDate: null ;
+    endDate: null  ;
+  }
+  render(){
+    return (
+      <div>
+        <input type="date"></input>
+        <input type="date"></input>
+      </div>
+    );
+  }
+}*/
