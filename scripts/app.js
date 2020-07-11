aea77a026c3c84cdd9c123058b5da5d5f87015e8
noecor <noeliacoronelg@hotmail.com>
2020-07-11 17:01:09 -0300

    agrego info CardList

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,7 @@ const App = () => {
       <h1></h1>
         <Header/>
         <Filters/>
-        <Card/>
+        <CardList/>
     </div>
   );
 }
--- scripts/cardList.js
+++ scripts/cardList.js
@@ -1,19 +1,20 @@
-class cardList extends React.Component {
-    {state= {
-        hoteles: [],
-    }
-    render() {
-        const cardComponents = hotelsData.map( h => (
-          <Card
-            hotelImageUrl={h.photo}
-            title={h.name}
-            description={h.description}
-            location={h.city,}{h.country}
-            rooms={h.rooms}
-            price={h.price}
-            key={h.name}
-            id={h.name}          
-          />
-        ));
-        return <div className="ui unstackable items">{cardComponents}</div>;
-    }
+class CardList extends React.Component {
+  state= {
+    hoteles: [],
+  }
+  render() {
+    const cardComponents = hotelsData.map( h => (
+      <Card
+        hotelImageUrl={h.photo}
+        title={h.name}
+        description={h.description}
+        location={h.city, h.country}
+        rooms={h.rooms}
+        price={h.price}
+        key={h.name}
+        id={h.name}          
+      />
+    ));
+    return (<div className="ui unstackable items">{cardComponents}</div>);
+}
+}
