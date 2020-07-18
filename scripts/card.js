e6e7948f5a58ff542e5245f7c56e3216c1587b33
noecor <noeliacoronelg@hotmail.com>
2020-07-17 23:04:26 -0300

    agrego estilos a Card

--- scripts/card.js
+++ scripts/card.js
@@ -1,47 +1,49 @@
-const selectedPrice = (price) =>{ 
-    switch (price) {
-        case 1:
-        return "$"
-        break;
-        case 2:
-        return "$$"
-        break;
-        case 3:
-        return "$$$"
-        break;
-        case 4:
-        return "$$$$"
-        break;
-        default:
-            console.log("no se imprimió selectedPrice")
-    }
+const selectedPrice = (price) => {
+  switch (price) {
+    case 1:
+      return "$";
+      break;
+    case 2:
+      return "$$";
+      break;
+    case 3:
+      return "$$$";
+      break;
+    case 4:
+      return "$$$$";
+      break;
+    default:
+      console.log("no se imprimió selectedPrice");
   }
+};
 class Card extends React.Component {
-    render(){
-        return (
-        <div className="wrapper-Card">
-            <div className="image">
-                <img src={this.props.hotelImageUrl} alt="hotel" width="250"/>
-            </div>
-            <div className="title">
-                <p>{this.props.title}</p> 
-            </div>
-            <div>
-                <p>{this.props.description}</p>
-            </div>
-            <div>
-                <i className="material-icons">room</i>
-                <p>{this.props.city}, {this.props.country}</p>
-            </div>
-            <div>
-                <i className="material-icons">hotel</i>
-                <p>{this.props.rooms} Habitaciones</p>
-            </div>
-            <div>
-                <p>{selectedPrice(this.props.price)}</p>
-            </div>
-            <button className="btn">Reservar</button>
+  render() {
+    return (
+      <div className="wrapper-Card">
+        <div className="image">
+          <img src={this.props.hotelImageUrl} alt="hotel" width="280" />
         </div>
-        );
-    }
+        <div className="title">
+          <p>{this.props.title}</p>
+        </div>
+        <div>
+          <p className="description">{this.props.description}</p>
+        </div>
+        <div className="button">
+          <i className="material-icons icon">room</i>
+          <p className="city">
+            {this.props.city}, {this.props.country}
+          </p>
+        </div>
+        <div className="button">
+          <i className="material-icons icon">hotel</i>
+          <p className="city">{this.props.rooms} Habitaciones</p>
+        </div>
+        <div>
+          <p className="price">{selectedPrice(this.props.price)}</p>
+        </div>
+        <button className="btn">Reservar</button>
+      </div>
+    );
+  }
 }
